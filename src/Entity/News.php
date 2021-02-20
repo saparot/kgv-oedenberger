1decd3dbe1a1b9433106322429ca609c6326ae15
Saparot <coding@saparot.io>
2021-02-20 13:50:34 +0100

    added User, News added easy-admin, dashboard added related admin controllers for user, news

---
+++ src/Entity/News.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NewsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NewsRepository::class)
+ */
+class News
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $desription;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $timeCreated;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $timeUpdated;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDesription(): ?string
+    {
+        return $this->desription;
+    }
+
+    public function setDesription(string $desription): self
+    {
+        $this->desription = $desription;
+
+        return $this;
+    }
+
+    public function getTimeCreated(): ?\DateTimeInterface
+    {
+        return $this->timeCreated;
+    }
+
+    public function setTimeCreated(\DateTimeInterface $timeCreated): self
+    {
+        $this->timeCreated = $timeCreated;
+
+        return $this;
+    }
+
+    public function getTimeUpdated(): ?\DateTimeInterface
+    {
+        return $this->timeUpdated;
+    }
+
+    public function setTimeUpdated(\DateTimeInterface $timeUpdated): self
+    {
+        $this->timeUpdated = $timeUpdated;
+
+        return $this;
+    }
+}
