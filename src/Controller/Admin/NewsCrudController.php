2de8fa51df823a576e1015ac3713135f8287126f
Saparot <coding@saparot.io>
2021-02-20 14:13:52 +0100

    added User, News added easy-admin, dashboard added related admin controllers for user, news

--- src/Controller/Admin/NewsCrudController.php
+++ src/Controller/Admin/NewsCrudController.php
@@ -5,13 +5,11 @@ namespace App\Controller\Admin;
 use App\Entity\News;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
-class NewsCrudController extends AbstractCrudController
-{
-    public static function getEntityFqcn(): string
-    {
+class NewsCrudController extends AbstractCrudController {
+
+    public static function getEntityFqcn (): string {
         return News::class;
     }
-
     /*
     public function configureFields(string $pageName): iterable
     {
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -5,13 +5,11 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
-class UserCrudController extends AbstractCrudController
-{
-    public static function getEntityFqcn(): string
-    {
+class UserCrudController extends AbstractCrudController {
+
+    public static function getEntityFqcn (): string {
         return User::class;
     }
-
     /*
     public function configureFields(string $pageName): iterable
     {
