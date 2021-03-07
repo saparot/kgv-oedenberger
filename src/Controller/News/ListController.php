c54df73c9e29396407f9f15f5ca45ef8e680f967
Saparot <coding@saparot.io>
2021-03-07 14:15:47 +0100

    added bundle FOSCKEditorBundle added simple draft for adding a news with ckeditor

--- src/Controller/News/ListController.php
+++ src/Controller/News/ListController.php
@@ -2,14 +2,25 @@
 
 namespace App\Controller\News;
 
+use App\Entity\News;
+use App\Entity\User;
 use App\Helper\BreadCrumbsChain;
 use App\Mixin\BreadCrumbMixin;
 use App\Mixin\LinkListMixin;
 use App\Mixin\PageviewMixin;
+use DateTime;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Date;
 
 class ListController extends AbstractController {
 
@@ -34,6 +45,27 @@ class ListController extends AbstractController {
      * @return Response
      */
     function index (Request $request): Response {
+        $newsForm = $this->createFormBuilder()->add('title', TextType::class, ['label' => 'Überschrift', 'required' => false,])->add('news', CKEditorType::class, [
+            'config' => ['toolbar' => 'full'],
+            'label' => 'die Neuigkeit',
+            'required' => true,
+        ])->add('speichern', SubmitType::class)->getForm();
+
+        $newsForm->handleRequest($request);
+
+        if ($newsForm->isSubmitted()) {
+            $data = $newsForm->getData();
+
+            $news = (new News())->setTitle($data['title'])->setDescription($data['news'])->setTimeCreated(new DateTime())->setTimeUpdated(new DateTime());
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($news);
+            $em->flush();
+
+            return $this->redirectToRoute('newsList');
+        }
+
+        return $this->assign('newsForm', $newsForm->createView())->renderPageView();
+
         return $this->renderPageView();
     }
 }
