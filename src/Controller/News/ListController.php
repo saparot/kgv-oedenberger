355715495d68b0d60c7bb37d773ddf9bd910bb5e
Saparot <coding@saparot.io>
2021-03-07 19:59:37 +0100

    added twig intl-extra small update newslist

--- src/Controller/News/ListController.php
+++ src/Controller/News/ListController.php
@@ -3,24 +3,20 @@
 namespace App\Controller\News;
 
 use App\Entity\News;
-use App\Entity\User;
 use App\Helper\BreadCrumbsChain;
 use App\Mixin\BreadCrumbMixin;
 use App\Mixin\LinkListMixin;
 use App\Mixin\PageviewMixin;
+use App\Repository\NewsRepository;
 use DateTime;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints\Date;
 
 class ListController extends AbstractController {
 
@@ -44,28 +40,39 @@ class ListController extends AbstractController {
      *
      * @return Response
      */
-    function index (Request $request): Response {
-        $newsForm = $this->createFormBuilder()->add('title', TextType::class, ['label' => 'Überschrift', 'required' => false,])->add('news', CKEditorType::class, [
+    function index (Request $request, NewsRepository $newsRepository): Response {
+        ;
+        $newsForm = $this->getNewsForm();
+        $newsForm->handleRequest($request);
+
+        if ($newsForm->isSubmitted()) {
+            return $this->save($newsForm);
+        }
+        $this->addNewsList($newsRepository);
+
+        return $this->assign('newsForm', $newsForm->createView())->renderPageView();
+    }
+
+    private function addNewsList (NewsRepository $newsRepository) {
+        $this->assign('newsList', $newsRepository->findAll());
+    }
+
+    private function getNewsForm (): FormInterface {
+        return $this->createFormBuilder()->add('title', TextType::class, ['label' => 'Überschrift', 'required' => false,])->add('news', CKEditorType::class, [
             'config' => ['toolbar' => 'full'],
             'label' => 'die Neuigkeit',
             'required' => true,
         ])->add('speichern', SubmitType::class)->getForm();
+    }
 
-        $newsForm->handleRequest($request);
+    private function save (FormInterface $newsForm) {
+        $data = $newsForm->getData();
 
-        if ($newsForm->isSubmitted()) {
-            $data = $newsForm->getData();
+        $news = (new News())->setTitle($data['title'])->setDescription($data['news'])->setTimeCreated(new DateTime())->setTimeUpdated(new DateTime());
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($news);
+        $em->flush();
 
-            $news = (new News())->setTitle($data['title'])->setDescription($data['news'])->setTimeCreated(new DateTime())->setTimeUpdated(new DateTime());
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($news);
-            $em->flush();
-
-            return $this->redirectToRoute('newsList');
-        }
-
-        return $this->assign('newsForm', $newsForm->createView())->renderPageView();
-
-        return $this->renderPageView();
+        return $this->redirectToRoute('newsList');
     }
 }
