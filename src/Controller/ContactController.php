d84108c5b9bf693facf9b8c74c8794b7b29366e3
Saparot <coding@saparot.io>
2021-04-05 11:42:04 +0200

    enriched contact form with relevant definitions, validations. provide from and to as env values

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,15 +3,18 @@
 namespace App\Controller;
 
 use App\Form\ContactForm;
-use App\Form\ExecutiveType;
 use App\Helper\BreadCrumbsChain;
 use App\Helper\Categories;
 use App\Mixin\BreadCrumbMixin;
 use App\Mixin\LinkListMixin;
 use App\Mixin\PageviewMixin;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController {
@@ -37,15 +40,16 @@ class ContactController extends AbstractController {
     /**
      * @Route("/contact", name="contact")
      * @param Request $request
+     * @param MailerInterface $mailer
      *
      * @return Response
      */
-    function index (Request $request): Response {
+    function index (Request $request, MailerInterface $mailer): Response {
         $form = $this->createForm(ContactForm::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+            $this->send($form, $mailer);
             $this->addFlash('success', 'Ihre Anfrage wurde erfolgreich gesendet');
 
             return $this->redirectToRoute('landingPage');
@@ -53,5 +57,54 @@ class ContactController extends AbstractController {
         $this->assign('form', $form->createView());
 
         return $this->renderPageView();
+    }
+
+    private function send (FormInterface $form, MailerInterface $mailer) {
+        $mailer->send($this->generateEmail($form));
+    }
+
+    private function generateEmail (FormInterface $form): Email {
+        return (new Email())->from($this->getFrom())
+            ->to($this->getTo())
+            ->subject($this->getSubject($form))
+            ->text($this->getBody($form))
+            ->addReplyTo($this->getEmail($form));
+    }
+
+    /**
+     * @return string
+     * @throws Exception
+     */
+    private function getTo (): string {
+        return $_ENV['CONTACT_FORM_TO'] ?? $this->throw("CONTACT_FORM_TO env value is not defined");
+    }
+
+    /**
+     * @return string
+     * @throws Exception
+     */
+    private function getFrom (): string {
+        return $_ENV['CONTACT_FORM_FROM'] ?? $this->throw("CONTACT_FORM_FROM env value is not defined");
+    }
+
+    /**
+     * @param string $error
+     *
+     * @throws Exception
+     */
+    private function throw (string $error) {
+        throw new Exception("unable to {$error}");
+    }
+
+    private function getEmail (FormInterface $form): string {
+        return $form->get('email')->getData();
+    }
+
+    private function getSubject (FormInterface $form): string {
+        return 'Anfrage: Test';
+    }
+
+    private function getBody (FormInterface $form): string {
+        return sprintf("%s hat eine Frage neue Anfrage gestellt:\n\n%s", $form->get('name')->getData(), $form->get('request')->getData());
     }
 }
--- src/Form/ContactForm.php
+++ src/Form/ContactForm.php
@@ -5,14 +5,40 @@ namespace App\Form;
 use App\Entity\Executive;
 use EWZ\Bundle\RecaptchaBundle\Form\Type\EWZRecaptchaType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ContactForm extends AbstractType {
 
     public function buildForm (FormBuilderInterface $builder, array $options) {
-        $builder->add('Name')->add('E-Mail-Adresse')->add('Anfrage', TextareaType::class);
+        $builder->add('name', TextType::class, [
+            'label_attr' => ['class' => 'formRequired'],
+            'constraints' => [
+                new NotBlank(),
+                new Length(['min' => 3]),
+            ],
+        ])->add('email', EmailType::class, [
+            'label_format' => 'E-Mail-Adresse',
+            'label_attr' => ['class' => 'formRequired'],
+            'constraints' => [
+                new NotBlank(),
+                new Length(['min' => 3]),
+                new Email(),
+            ],
+        ])->add('request', TextareaType::class, [
+            'label_attr' => ['class' => 'formRequired'],
+            'label_format' => 'Ihre Anfrage',
+            'constraints' => [
+                new NotBlank(),
+                new Length(['min' => 10]),
+            ],
+        ]);
         //using reCaptcha
         //https://packagist.org/packages/excelwebzone/recaptcha-bundle
         $builder->add('recaptcha', EWZRecaptchaType::class, [
